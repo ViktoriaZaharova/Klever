8e1815b47c03402d8f3f1e294c65979dc4d502cf
Виктория Захарова <zaharova181114@gmail.com>
2024-05-11 22:52:45 +0300

    report page

--- js/common.js
+++ js/common.js
@@ -14,3 +14,18 @@ $('.btn-burger').on('click', function () {
 $('.sidebar-wrapper__close').on('click', function () {
 	$('.sidebar-wrapper').fadeOut();
 });
+
+Fancybox.bind("[data-fancybox]", {
+	// Your custom options
+});
+
+
+$('.btn-toggle-reports').on('click', function (e) {
+	e.preventDefault();
+	$(this).parents('.reports-wrapper').find('.report-col:hidden').slice(0, 3).slideDown();
+
+	var onBlock = $(this).parents('.reports-wrapper').find('.report-col:hidden').length;
+	if (onBlock <= 0) {
+		$(this).hide();
+	}
+});
