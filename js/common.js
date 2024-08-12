cc7729b10fb6ae09ab661e9b3443ff5d13937079
Viktoria Zaharova <zaharova181114@gmail.com>
2024-08-12 23:50:45 +0300

    + tabs

--- js/common.js
+++ js/common.js
@@ -2,9 +2,40 @@ $('[name="phone"]').mask('+7 (999) 999-99-99');
 
 $('.home-slider').slick({
 	slidesToShow: 1,
+	swipeToSlide: true,
 	arrows: false,
 	dots: true,
 	fade: true,
+});
+
+$('.gallery-slider-portfolio').slick({
+	slidesToShow: 3,
+	swipeToSlide: true,
+	variableWidth: true,
+	arrows: true,
+	appendArrows: '.gallery-slider-portfolio__nav',
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev-arrow"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next-arrow"></use></svg></button>'
+});
+
+$('.gallery-slider-video').slick({
+	slidesToShow: 3,
+	swipeToSlide: true,
+	variableWidth: true,
+	arrows: true,
+	appendArrows: '.gallery-slider-video__nav',
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev-arrow"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next-arrow"></use></svg></button>'
+});
+
+$('.gallery-slider-documents').slick({
+	slidesToShow: 4,
+	swipeToSlide: true,
+	variableWidth: true,
+	arrows: true,
+	appendArrows: '.gallery-slider-documents__nav',
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev-arrow"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next-arrow"></use></svg></button>'
 });
 
 $('.btn-burger').on('click', function () {
